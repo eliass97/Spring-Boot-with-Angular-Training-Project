4651ac5648d1a9f7200199f1095977185bc4a3f0
Ilias Settas <ilias.settas@eurodyn.com>
2020-10-09 12:07:53 +0300

    Scheduling with Spring (final version)

--- src/main/java/com/example/demo/scheduled/CountryCounterJob.java
+++ src/main/java/com/example/demo/scheduled/CountryCounterJob.java
@@ -4,9 +4,11 @@ import com.example.demo.service.CountryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
+@Component
 public class CountryCounterJob {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CountryCounterJob.class);
@@ -17,7 +19,7 @@ public class CountryCounterJob {
         this.countryService = countryService;
     }
 
-    @Scheduled(fixedDelay = 3000000)
+    @Scheduled(fixedDelay = 10000)
     public void recordNumberOfCountries() {
         LOGGER.info("Recorded number of countries on {} : {}", LocalDateTime.now(), countryService.getNumberOfCountries());
     }
--- src/main/java/com/example/demo/scheduled/PersonCounterJob.java
+++ src/main/java/com/example/demo/scheduled/PersonCounterJob.java
@@ -4,9 +4,11 @@ import com.example.demo.service.PersonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
+@Component
 public class PersonCounterJob {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PersonCounterJob.class);
@@ -17,7 +19,7 @@ public class PersonCounterJob {
         this.personService = personService;
     }
 
-    @Scheduled(fixedDelay = 3000000)
+    @Scheduled(fixedDelay = 10000)
     public void recordNumberOfPersons() {
         LOGGER.info("Recorded number of persons on {} : {}", LocalDateTime.now(), personService.getNumberOfPersons());
     }
