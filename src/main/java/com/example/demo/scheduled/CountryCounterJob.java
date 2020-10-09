d4209400e81ad7281c3b5e8d48dbc0718f2bf7b8
Ilias Settas <ilias.settas@eurodyn.com>
2020-10-09 14:01:11 +0300

    Quartz Properties Fix
    - fix properties definition

--- src/main/java/com/example/demo/scheduled/CountryCounterJob.java
+++ src/main/java/com/example/demo/scheduled/CountryCounterJob.java
@@ -1,6 +1,7 @@
 package com.example.demo.scheduled;
 
 import com.example.demo.service.CountryService;
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 
 @Component
+@DisallowConcurrentExecution
 public class CountryCounterJob extends QuartzScheduledJob {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CountryCounterJob.class);
@@ -16,7 +18,7 @@ public class CountryCounterJob extends QuartzScheduledJob {
     private final CountryService countryService;
 
     private final String JOB_NAME = "countryCounterJob";
-    private final String CRON_EXPRESSION = "0 0/5 * * * ?";
+    private final String CRON_EXPRESSION = "0/5 * * * * ?";
 
     public CountryCounterJob(CountryService countryService) {
         this.countryService = countryService;
@@ -24,6 +26,11 @@ public class CountryCounterJob extends QuartzScheduledJob {
 
     @Override
     public void executeInternal(JobExecutionContext jec) {
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            LOGGER.error("sth got seriously fucked, shouldn't be here", e);
+        }
         LOGGER.info("{} : Recorded number of countries on : {}", LocalDateTime.now(), countryService.getNumberOfCountries());
     }
 
--- src/main/java/com/example/demo/scheduled/PersonCounterJob.java
+++ src/main/java/com/example/demo/scheduled/PersonCounterJob.java
@@ -1,6 +1,7 @@
 package com.example.demo.scheduled;
 
 import com.example.demo.service.PersonService;
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 
 @Component
+@DisallowConcurrentExecution
 public class PersonCounterJob extends QuartzScheduledJob {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PersonCounterJob.class);
@@ -16,7 +18,7 @@ public class PersonCounterJob extends QuartzScheduledJob {
     private final PersonService personService;
 
     private final String JOB_NAME = "personCounterJob";
-    private final String CRON_EXPRESSION = "0 0/5 * * * ?";
+    private final String CRON_EXPRESSION = "0 0/10 * * * ?";
 
     public PersonCounterJob(PersonService personService) {
         this.personService = personService;
